8eb84c9dc8cb4f2888497c53652568687455d3c7
Astralroth <francosandoval003@gmail.com>
2022-06-03 15:39:02 -0400

    Avance sumativo

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from core.views import index, login, redirect_to_root
+from core.views import buscar_sucursal, index, login, redirect_to_root, signin
 
 
 urlpatterns = [
     path('', redirect_to_root, name='redirect_to_root'),
     path('index/', index, name='index'),
     path('login/', login, name='login'),
+    path('signin/', signin, name='signin'),
+    path('buscar_sucursal/', buscar_sucursal, name='buscar_sucursal'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,10 +4,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def redirect_to_root (request):
-    return HttpResponseRedirect('login')
+    return HttpResponseRedirect('index')
 
 def index(request):
     return render(request, 'core/index.html')
 
 def login(request):
     return render(request, 'core/cred/login.html')
+
+def signin(request):
+    return render(request, 'core/cred/signin.html')
+
+def buscar_sucursal(request):
+    return render(request, 'core/buscar_sucursal.html')
